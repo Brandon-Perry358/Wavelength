8c5b1856dae85321898921a0399c2ed83beb7583
Brandon-Perry358 <55165760+Brandon-Perry358@users.noreply.github.com>
2023-03-26 02:12:51 -0400

    More small fixes and theme change

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def play_audio(queue):
                 queue.put("play playlist")
             elif not player.playing and player.active:
                 player.resume()
+            else:
+                pass
         elif message == "stop":
             player.stop()
             playlist.clear()
@@ -43,16 +45,22 @@ def play_audio(queue):
                 player.load_file(currently_playing[0])
                 player.play()
             if not player.active:
-                #read the playlist and place the first song in the currently_playing list
-                currently_playing.append(playlist[0])
-                #remove the first song from the playlist
-                playlist.pop(0)
-                #play the song
-                player.load_file(currently_playing[0])
-                player.play()
+                #if playlist is empty, do nothing
+                if len(playlist) == 0:
+                    pass
+                elif len(playlist) >= 1:
+                    #read the playlist and place the first song in the currently_playing list
+                    currently_playing.append(playlist[0])
+                    #remove the first song from the playlist
+                    playlist.pop(0)
+                    #play the song
+                    player.load_file(currently_playing[0])
+                    player.play()
+                else:
+                    pass
         elif message == "next song":
             #if the next song is the last song in the playlist, stop the player
-            if len(playlist) == 0:
+            if len(playlist) <= 0:
                 player.stop()
                 playlist.clear()
                 currently_playing.clear()
@@ -98,28 +106,35 @@ def gui(queue):
     window.setWindowTitle("Wavelength Audio Player")
     window.setWindowIcon(QtGui.QIcon("WavelengthIcon.png"))
     #change background color to aqua blue
-    #window.setStyleSheet("background-color: #00FFFF;")
+    window.setStyleSheet("background-color: black;")
     window.setGeometry(100, 100, 775, 500)
 
     # make program name in corner
     wavelengthLabel = QtWidgets.QLabel(window)
     wavelengthLabel.setText("Wavelength")
+    #set text color to red
+    wavelengthLabel.setStyleSheet("color: #39ff14;")
     wavelengthLabel.move(10, 0)
 
     # code for the add to playlist button
     addToPlaylistButton = QtWidgets.QPushButton("Browse", window)
+    #set the color of the button to red
+    addToPlaylistButton.setStyleSheet("background-color: #39ff14;")
     addToPlaylistButton.clicked.connect(lambda: queue.put("add to playlist"))
     addToPlaylistButton.move(75, 0)
 
     # code for creating the stop button
     stopButton = QtWidgets.QPushButton("Stop", window)
     stopButton.clicked.connect(lambda: queue.put("stop"))
-    # move the button below the play/pause button
+    # set the color of the button to red
+    stopButton.setStyleSheet("background-color: #39ff14;")
     stopButton.move(175, 0)
 
     albumArt = QtWidgets.QLabel(window)
     albumPixmap = QtGui.QPixmap()
     albumArt.setPixmap(QtGui.QPixmap("WavelengthArt.png"))
+    #Make a green border around the album art
+    albumArt.setStyleSheet("border: 2px solid #39ff14;")
     albumArt.setScaledContents(True)
     albumArt.setGeometry(20, 40, 400, 360)
 
@@ -127,35 +142,51 @@ def gui(queue):
     currintPosLabel = QtWidgets.QLabel(window)
     currintPosLabel.setGeometry(450, 50, 30, 30)
     currintPosLabel.setText("0:00")
+    #set text color to red
+    currintPosLabel.setStyleSheet("color: #39ff14;")
 
     # make seek bar
     seekBar = QtWidgets.QSlider(window)
     seekBar.setOrientation(QtCore.Qt.Vertical)
     seekBar.setGeometry(450, 75, 25, 300)
     seekBar.setInvertedAppearance(True)
+    #Set the color of the seek bar handle to green and show past progress as green in the bar
+    seekBar.setStyleSheet("QSlider::handle:vertical {background-color: #39ff14;}")
+    #seekBar.valueChanged.connect(lambda: queue.put("seek " + (seekBar.value()).__str__()))
 
     # show total song time
     trackLengthLabel = QtWidgets.QLabel(window)
     trackLengthLabel.setGeometry(450, 375, 30, 30)
-    trackLengthLabel.setText("1:34")
+    trackLengthLabel.setText("0:00")
+    #set text color to red
+    trackLengthLabel.setStyleSheet("color: #39ff14;")
 
     #make a list widget to display the playlist
     playlistWidget = QtWidgets.QListWidget(window)
     playlistWidget.setGeometry(500, 40, 250, 360)
-    #Display the contents of the playlist in the list widget
+    #set the color of the widget to white
+    playlistWidget.setStyleSheet("background-color: white;")
 
     # make song name under art
     songLabel = QtWidgets.QLabel(window)
-    songLabel.setGeometry(25, 400, 400, 25)
+    songLabel.setGeometry(20, 400, 400, 25)
     songLabel.setText("Wavelength")
     songLabel.setAlignment(QtCore.Qt.AlignCenter)
-    songLabel.setFont(QtGui.QFont("Helvetica", 17, QtGui.QFont.Bold))
+    #set text box color to transparent
+    songLabel.setStyleSheet("background-color: transparent;" + "color: #39ff14")
+    #set text color to red
+    #songLabel.setStyleSheet("color: red;")
+    songLabel.setFont(QtGui.QFont("Helvetica", 15, QtGui.QFont.Bold))
 
     # make artist name under song title
     artistLabel = QtWidgets.QLabel(window)
-    artistLabel.setGeometry(25, 415, 400, 40)
+    artistLabel.setGeometry(20, 415, 400, 40)
     artistLabel.setText("Spack & Brandon P.")
     artistLabel.setAlignment(QtCore.Qt.AlignCenter)
+    #set text box color to transparent
+    artistLabel.setStyleSheet("background-color: transparent;" + "color: #39ff14")
+    #set text color to red
+    #artistLabel.setStyleSheet("color: red;")
     artistLabel.setFont(QtGui.QFont("Helvetica", 11))
 
     # create button
@@ -163,7 +194,9 @@ def gui(queue):
     skipBackButton.clicked.connect(lambda: queue.put("previous song"))
     skipBackButton.move(20, 450)
     skipBackButton.setGeometry(20, 450, 133, 35)
-    skipBackButton.setStyleSheet("background-image : url(skipBackButton.png); background-repeat : no-repeat; background-position : center;")
+    skipBackButton.setStyleSheet("background-color: #39ff14;" + "background-image : url(skipBackButton.png); background-repeat : no-repeat; background-position : center;")
+    # set the color of the button to red
+    #skipBackButton.setStyleSheet("background-color: red;")
 
     # code for creating the button
     playPauseButton = QtWidgets.QPushButton("", window)
@@ -172,14 +205,20 @@ def gui(queue):
     playPauseButton.move(125, 450)
     playPauseButton.setGeometry(153, 450, 134, 35)
     #add image to playpause button and center it
-    playPauseButton.setStyleSheet("background-image : url(playPauseButton.png); background-repeat : no-repeat; background-position : center;")
+    playPauseButton.setStyleSheet("background-color: #39ff14;" + "background-image : url(playPauseButton.png); background-repeat : no-repeat; background-position : center;")
+    # set the color of the button to red
+    #playPauseButton.setStyleSheet("background-color: red;")
 
     # create button
     skipForwardButton = QtWidgets.QPushButton("", window)
     skipForwardButton.clicked.connect(lambda: queue.put("next song"))
     skipForwardButton.move(225, 450)
     skipForwardButton.setGeometry(287, 450, 133, 35)
-    skipForwardButton.setStyleSheet("background-image : url(skipForwardButton.png); background-repeat : no-repeat; background-position : center;")
+    skipForwardButton.setStyleSheet("background-color: #39ff14;" + "background-image : url(skipForwardButton.png); background-repeat : no-repeat; background-position : center;")
+    # set the color of the button to red
+    #skipForwardButton.setStyleSheet("background-color: red;")
+    #set the color of the button to neon green
+    #skipForwardButton.setStyleSheet("background-color: #39ff14;")
 
     # make volume bar
     volBar = QtWidgets.QSlider(window)
@@ -190,6 +229,8 @@ def gui(queue):
     volBar.setValue(50)
     #control the volume of the player
     volBar.valueChanged.connect(lambda: queue.put("volume " + (volBar.value()).__str__()))
+    #set the color of the slider bar to neon green
+    volBar.setStyleSheet("QSlider::groove:horizontal {border: 1px solid #bbb; background: white; height: 10px; border-radius: 4px;}" + "QSlider::sub-page:horizontal {background: #39ff14; border: 1px solid #777; height: 10px; border-radius: 4px;}" + "QSlider::add-page:horizontal {background: #fff; border: 1px solid #777; height: 10px; border-radius: 4px;}" + "QSlider::handle:horizontal {background: #39ff14; border: 1px solid #777; width: 13px; margin-top: -2px; margin-bottom: -2px; border-radius: 4px;}" + "QSlider::handle:horizontal:hover {background: #39ff14; border: 1px solid #444; width: 13px; margin-top: -2px; margin-bottom: -2px; border-radius: 4px;}" + "QSlider::sub-page:horizontal:disabled {background: #bbb; border-color: #999;}" + "QSlider::add-page:horizontal:disabled {background: #eee; border-color: #999;}" + "QSlider::handle:horizontal:disabled {background: #eee; border: 1px solid #aaa; border-radius: 4px;}")
 
     timer = QtCore.QTimer()
     timer.timeout.connect(lambda: update_playlist(playlistWidget, playlist))
@@ -270,12 +311,15 @@ def update_art(albumArt, albumPixmap,  currently_playing):
 
 def update_end_time(trackLengthLabel, currently_playing):
     if len(currently_playing) == 0:
-        pass
+        trackLengthLabel.setText("0:00")
     else:
         song_tag = tinytag.TinyTag.get(currently_playing[0])
         min = int(song_tag.duration % 3600 / 60)
         sec = int(song_tag.duration % 3600 % 60)
-        trackLengthLabel.setText(str(min) + ":" + str(sec))
+        if sec < 10:
+            trackLengthLabel.setText(str(min) + ":0" + str(sec))
+        else:
+            trackLengthLabel.setText(str(min) + ":" + str(sec))
 
 queue = Queue()
 audio_thread = threading.Thread(target=play_audio, args=(queue,))
